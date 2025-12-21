748d173d270fea3e4e2d1945bfbc5f5bea84205d
Jerome Rodrigo <jeromerodrigo06@gmail.com>
2025-12-20 20:53:41 -0500

    basic training setup

--- cpp_neural_engine/main.cpp
+++ cpp_neural_engine/main.cpp
@@ -1,7 +1,30 @@
 #include <iostream>
 
-
-
 int main() {
-    return 0;
+    double x = 2;
+    double y = 5;
+
+    double w = 1;
+    double b = 1;
+
+    double learning_rate = 0.01;
+    int epochs = 1000;
+    
+    for(int j = 0; j < epochs; j++) {
+       
+        double y_pred = w * x + b;
+       
+        double loss = (y - y_pred) * (y - y_pred);
+
+        double dl_dw = -2 * (y - y_pred) * x;
+        double dl_db = -2 * (y - y_pred);
+
+        w = w - learning_rate * dl_dw;
+        b = b - learning_rate * dl_db;
+
+        if (j % 100 == 0) {
+            std::cout << "Epoch " << j << ": Loss = " << loss 
+          << ", w = " << w << ", b = " << b << std::endl;
+        }
+    }
 }
