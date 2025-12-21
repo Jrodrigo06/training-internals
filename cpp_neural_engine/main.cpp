ca946c77448eae245c6436bae91692482cdd292b
Jerome Rodrigo <jeromerodrigo06@gmail.com>
2025-12-21 03:12:49 -0500

    Added structure, making a dense layer struct to serpeate out responsibilities, implementing caching and chain rule derivatives to update the weights and bias

--- cpp_neural_engine/main.cpp
+++ cpp_neural_engine/main.cpp
@@ -1,12 +1,32 @@
 #include <iostream>
 
+struct DenseLayer {
+    double w;
+    double b;
+    double cached_input;
+    
+    DenseLayer(double init_w, double init_b) {
+        w = init_w;
+        b = init_b;
+    }
+    
+    double forward(double x) {
+        cached_input = x;
+        return w * cached_input + b;
+    }
+    
+    void backward(double grad_from_loss, double learning_rate) {
+        w = w - learning_rate * (cached_input * grad_from_loss);
+        b = b - learning_rate * grad_from_loss;
+    }
+};
+
 int main() {
 
     double x_data[] = {1.0, 2.0, 3.0, 4.0};
     double y_data[] = {3.0, 5.0, 7.0, 9.0};
 
-    double w = 1;
-    double b = 1;
+    DenseLayer layer(1.0, 1.0);
 
     double learning_rate = 0.01;
     int epochs = 1000;
@@ -19,16 +39,14 @@ int main() {
             double x = x_data[k];
             double y = y_data[k];
 
-            double y_pred = w * x + b;
+            double y_pred = layer.forward(x);
 
             double loss = (y - y_pred) * (y - y_pred);
             total_loss += loss;
 
-            double dl_dw = -2 * (y - y_pred) * x;
-            double dl_db = -2 * (y - y_pred);
+            double dl_dy_pred = -2 * (y - y_pred);
 
-            w = w - learning_rate * dl_dw;
-            b = b - learning_rate * dl_db;
+            layer.backward(dl_dy_pred, learning_rate);
         }
 
         total_loss = total_loss / 4;
@@ -37,7 +55,7 @@ int main() {
 
         if (j % 100 == 0) {
             std::cout << "Epoch " << j << ": Avg Loss = " << total_loss 
-          << ", w = " << w << ", b = " << b << std::endl;
+          << ", w = " << layer.w << ", b = " << layer.b << std::endl;
         }
     }
 }
